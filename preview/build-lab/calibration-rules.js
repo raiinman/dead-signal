8ceedabfee5a838b3527219e22ebdac256ca9d1e
ShiroMaō Studios <42227255+raiinman@users.noreply.github.com>
2026-08-10 16:49:50 -0700

    Add calibration RNG rules

---
+++ preview/build-lab/calibration-rules.js
@@ -0,0 +1,28 @@
+window.DS_CALIBRATION_RULES={
+  version:'1.4.4',
+  main:{
+    Rare:[18,25],
+    Epic:[26,33],
+    Legendary:[34,50]
+  },
+  secondary:{
+    Rare:[
+      {id:'weakspot',name:'Weakspot DMG',min:12,max:18},
+      {id:'crit_rate',name:'Crit Rate',min:8,max:12},
+      {id:'elemental',name:'Elemental DMG',min:12,max:18},
+      {id:'crit_dmg',name:'Crit DMG',min:20,max:30}
+    ],
+    Epic:[
+      {id:'weakspot',name:'Weakspot DMG',min:15,max:21},
+      {id:'crit_rate',name:'Crit Rate',min:10,max:14},
+      {id:'elemental',name:'Elemental DMG',min:12,max:18},
+      {id:'crit_dmg',name:'Crit DMG',min:25,max:35}
+    ],
+    Legendary:[
+      {id:'weakspot',name:'Weakspot DMG',min:18,max:24},
+      {id:'crit_rate',name:'Crit Rate',min:12,max:16},
+      {id:'elemental',name:'Elemental DMG',min:15,max:20},
+      {id:'crit_dmg',name:'Crit DMG',min:30,max:40}
+    ]
+  }
+};
